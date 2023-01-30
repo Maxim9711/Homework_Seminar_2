4e46b525b77ee35ae509c6460f0ef9d884af36fd
Maxim <Bruce.97@mail.ru>
2023-01-30 07:16:35 +0700

    task 13

---
+++ Задача_13/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int size = 0;
+int numUsed = num;
+
+// Определяем количество разрядов
+while(numUsed != 0){
+    numUsed = numUsed/10;
+    size++;
+}
+
+if (size > 2){
+    // готовим делитель для выделения третьего числа
+    int div = 10;
+    for (int i = 0; i < size - 4; i++){
+        div = div * 10;
+    }
+    Console.WriteLine($"Третье число: {num/(div)%10}");
+}
+else
+    Console.WriteLine($"Число двухзначное");

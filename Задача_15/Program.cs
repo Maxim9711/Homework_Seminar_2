b2e58c6868ab72c9b1682dea3f44f106b4e76fa5
Maxim <Bruce.97@mail.ru>
2023-01-30 07:16:41 +0700

    task 15

---
+++ Задача_15/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите цифру, обозначающую день недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 6 | num == 7){
+    Console.WriteLine("Выходной!");
+}
+else
+    Console.WriteLine("Точно не выходной!");
